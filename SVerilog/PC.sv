a8623567b3e0953ff73ccd89a4236d00aeac1ecb
gustavo-sen <you@example.com>
2025-09-12 16:14:49 -0300

    init

---
+++ SVerilog/PC.sv
@@ -0,0 +1,19 @@
+module program_counter (
+    input  logic        clk,
+    input  logic        reset,
+    input  logic        load,    // carrega valor de operação em pc_in
+    input  logic        inc,     // incrementa contador
+    input  logic [7:0] pc_in,    // operação 
+    output logic [7:0] pc_out     
+);
+
+    always_ff @(posedge clk or posedge reset) begin
+        if (reset)
+            pc_out <= 8'd0;
+        else if (load)
+            pc_out <= pc_in;
+        else if (inc)
+            pc_out <= pc_out + 8'd1;
+    end
+
+endmodule
