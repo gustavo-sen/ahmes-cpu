ff6fffd00e97f96658a032882b11026ed40c52f0
gustavo-sen <no-email>
2025-10-26 00:26:29 -0300

    feat: Added multiplex memory write and SPI sync

--- VHDL/spi_loader.vhd
+++ VHDL/spi_loader.vhd
@@ -4,49 +4,83 @@ USE ieee.numeric_std.all;
 
 ENTITY spi_loader IS
     PORT (
-        clk           				: IN  STD_LOGIC;
-        
-        spi_sck       			: IN  STD_LOGIC; -- Clock do SPI
-        spi_ss        			: IN  STD_LOGIC; -- Slave Select (ativo em '0')
-        spi_mosi      			: IN  STD_LOGIC; -- Master Out, Slave In
-        
-        spi_addr_bus  		: OUT unsigned(7 downto 0);
-        spi_data_out  		: OUT unsigned(7 downto 0);
-        spi_mem_write 		: OUT STD_LOGIC
+        clk           : IN  STD_LOGIC; 
+        spi_sck       : IN  STD_LOGIC;  
+        spi_ss        : IN  STD_LOGIC;  
+        spi_mosi      : IN  STD_LOGIC;
+		  spi_data_out  : OUT unsigned(7 downto 0);
+		  spi_addr_bus  : OUT unsigned(7 downto 0);
+        spi_mem_write : OUT STD_LOGIC
     );
 END ENTITY spi_loader;
 
 ARCHITECTURE rtl OF spi_loader IS
-    SIGNAL bit_counter   : unsigned(2 downto 0);
-    SIGNAL shift_reg     : unsigned(7 downto 0);
-    SIGNAL addr_counter  : unsigned(7 downto 0) := (others => '0');
-    SIGNAL sck_old       : STD_LOGIC := '0';
+
+    SIGNAL spi_domain_shift_reg : unsigned(7 downto 0) := (others => '0');
+    SIGNAL spi_domain_bit_cnt   : unsigned(2 downto 0) := (others => '0');
+
+    SIGNAL addr_counter         : unsigned(7 downto 0) := (others => '0');
+    
+    SIGNAL data_valid           : STD_LOGIC := '0';
+    SIGNAL disable_spi          : STD_LOGIC := '0'; 
+
+    SIGNAL spi_ss_sync_p        : STD_LOGIC;
+    SIGNAL spi_ss_sync          : STD_LOGIC;
+
 BEGIN
 
-    PROCESS(clk)
+
+    spi_capture_proc : PROCESS(spi_sck, spi_ss)
     BEGIN
-        IF rising_edge(clk) THEN
-            sck_old <= spi_sck;
-            spi_mem_write <= '0'; -- mem_write é um pulso, o padrão é '0'
-
-            IF spi_ss = '1' THEN -- Se não estiver selecionado, reseta os contadores
-                bit_counter  <= (others => '0');
-                addr_counter <= (others => '0');
-            ELSE -- Modo de carregamento ativo (spi_ss = '0')
-
-                IF spi_sck = '1' AND sck_old = '0' THEN
-                    shift_reg <= shift_reg(6 downto 0) & spi_mosi;
-                    bit_counter <= bit_counter + 1;
-
-                    IF bit_counter = "111" THEN 					-- Após 8 bits (1 byte)
-                        spi_mem_write <= '1';						-- Gera o pulso de escrita
-                        spi_data_out  <= shift_reg; 				-- Coloca o byte no barramento de dados
-                        spi_addr_bus  <= addr_counter; 		-- Coloca o endereço atual no barramento
-                        addr_counter  <= addr_counter + 1; 	-- Prepara o endereço para o próximo byte
-                    END IF;
+        IF spi_ss = '1' THEN 
+            spi_domain_bit_cnt <= (others => '0');
+            data_valid         <= '0';
+        ELSIF rising_edge(spi_sck) THEN
+            IF spi_ss = '0' AND disable_spi = '0' THEN 
+                spi_domain_shift_reg <= spi_domain_shift_reg(6 downto 0) & spi_mosi;
+                
+                IF spi_domain_bit_cnt = "111" THEN
+                    data_valid         <= '1'; 
+                    spi_domain_bit_cnt <= (others => '0');
+                ELSE
+                    spi_domain_bit_cnt <= spi_domain_bit_cnt + 1;
                 END IF;
             END IF;
         END IF;
-    END PROCESS;
+    END PROCESS spi_capture_proc;
+
+    spi_ss_sync_proc : PROCESS(clk)
+    BEGIN
+        IF rising_edge(clk) THEN
+            spi_ss_sync_p <= spi_ss;
+            spi_ss_sync   <= spi_ss_sync_p;
+        END IF;
+    END PROCESS spi_ss_sync_proc;
+
+    system_clock_proc : PROCESS(clk)
+    BEGIN
+        IF rising_edge(clk) THEN
+            spi_mem_write <= '0';
+
+            IF spi_ss_sync = '1' THEN
+                addr_counter <= (others => '0');
+                disable_spi  <= '0'; 
+
+            ELSE
+                IF disable_spi = '1' THEN
+                    disable_spi <= '0'; 
+                ELSIF data_valid = '1' THEN 
+                    spi_data_out  <= spi_domain_shift_reg; 
+                    
+                    spi_addr_bus  <= addr_counter;
+                    spi_mem_write <= '1';
+                    
+                    addr_counter  <= addr_counter + 1;
+                    
+                    disable_spi   <= '1'; 
+                END IF;
+            END IF;
+        END IF;
+    END PROCESS system_clock_proc;
 
 END ARCHITECTURE rtl;
--- VHDL/top_ahmes.vhd
+++ VHDL/top_ahmes.vhd
@@ -2,47 +2,40 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
 
--- =============================================================================
--- == ENTIDADE DE TOPO                                                        ==
--- =============================================================================
-ENTITY top_entity IS
+ENTITY top_ahmes IS
     PORT (
-        clk          		: IN  STD_LOGIC;
-        reset        		: IN  STD_LOGIC;
-        btns         		: IN  unsigned(3 DOWNTO 0); -- btns(0) será usado para habilitar/desabilitar gravação
-        leds         		: OUT unsigned(3 DOWNTO 0);
+        clk          	: IN  STD_LOGIC;
+        reset        	: IN  STD_LOGIC;
+        btns         	: IN  unsigned(3 DOWNTO 0); -- btns(0) (habilitar/desabilitar gravação)
+        leds         	: OUT unsigned(3 DOWNTO 0);
         ERROR        	: OUT STD_LOGIC;
-        -- Entradas SPI
+		  Cout				: OUT STD_LOGIC;
         spi_sck      	: IN  STD_LOGIC;
-        spi_ss       		: IN  STD_LOGIC;
+        spi_ss       	: IN  STD_LOGIC;
         spi_mosi     	: IN  STD_LOGIC
     );
-END ENTITY top_entity;
+END ENTITY top_ahmes;
 
-ARCHITECTURE structural OF top_entity IS
+ARCHITECTURE structural OF top_ahmes IS
 
-    -- =========================================================================
-    -- == DECLARAÇÃO DOS COMPONENTES                                          ==
-    -- =========================================================================
-
-    -- Componente da Unidade de Controle (Processador)
+    -- Componente da Unidade de Controle
     COMPONENT ahmes_uc IS
         PORT (
-            address_bus : OUT unsigned(7 DOWNTO 0);
-            data_in     : IN  unsigned(7 DOWNTO 0);
-            data_out    : OUT unsigned(7 DOWNTO 0);
-            mem_write   : OUT std_logic;
-            clk         : IN  std_logic;
-            reset       : IN  std_logic;
-            ERROR       : OUT STD_LOGIC;
-            btns        : IN  unsigned(3 DOWNTO 0);
-            leds        : OUT unsigned(3 DOWNTO 0);
-            OPERACAO    : OUT unsigned(3 DOWNTO 0);
-            OPER_A      : OUT unsigned(7 DOWNTO 0);
-            OPER_B      : OUT unsigned(7 DOWNTO 0);
-            RESULT      : IN  unsigned(7 DOWNTO 0);
-            Cout        : OUT STD_LOGIC;
-            N, Z, C, B, V : IN STD_LOGIC
+            address_bus 	: OUT unsigned(7 DOWNTO 0);
+            data_in     	: IN  unsigned(7 DOWNTO 0);
+            data_out    	: OUT unsigned(7 DOWNTO 0);
+            mem_write   	: OUT std_logic;
+            clk         	: IN  std_logic;
+            reset       	: IN  std_logic;
+            ERROR       	: OUT STD_LOGIC;
+            btns        	: IN  unsigned(3 DOWNTO 0);
+            leds        	: OUT unsigned(3 DOWNTO 0);
+            OPERACAO    	: OUT unsigned(3 DOWNTO 0);
+            OPER_A      	: OUT unsigned(7 DOWNTO 0);
+            OPER_B      	: OUT unsigned(7 DOWNTO 0);
+            RESULT      	: IN  unsigned(7 DOWNTO 0);
+            Cout        	: OUT STD_LOGIC;
+            N, Z, C, B, V 	: IN STD_LOGIC
         );
     END COMPONENT ahmes_uc;
 
@@ -83,18 +76,13 @@ ARCHITECTURE structural OF top_entity IS
         );
     END COMPONENT spi_loader;
 
-    -- =========================================================================
-    -- == SINAIS DE INTERLIGAÇÃO                                              ==
-    -- =========================================================================
 
-    -- Sinais de comunicação entre UC e Memória
-    signal s_address_bus : unsigned(7 DOWNTO 0);
-    signal s_data_to_mem : unsigned(7 DOWNTO 0); -- Saída de dados da UC para a Memória
+    signal s_address_bus, uc_address_bus : unsigned(7 DOWNTO 0);
+    signal s_data_to_mem, uc_data_to_mem : unsigned(7 DOWNTO 0); -- Saída de dados da UC para a Memória
     signal s_data_from_mem : unsigned(7 DOWNTO 0); -- Saída de dados da Memória para a UC
-    signal s_mem_write, alu_mem_write   : std_logic;
-	signal s_mem_clk: std_logic;
+    signal s_mem_write, uc_mem_write   : std_logic;
+	 signal s_mem_clk: std_logic;
 
-    -- Sinais de comunicação entre UC e ALU
     signal s_operacao    : unsigned(3 DOWNTO 0);
     signal s_oper_a      : unsigned(7 DOWNTO 0);
     signal s_oper_b      : unsigned(7 DOWNTO 0);
@@ -102,26 +90,19 @@ ARCHITECTURE structural OF top_entity IS
     signal s_cin         : std_logic; -- Conecta Cout da UC com Cin da ALU
     signal s_n, s_z, s_c, s_b, s_v : std_logic; -- Flags da ALU para a UC
 
-    -- Sinais de SPI Loader
-    signal spi_loader_addr_bus : unsigned(7 DOWNTO 0);
     signal spi_loader_data_out : unsigned(7 DOWNTO 0);
+    signal spi_loader_addr_bus : unsigned(7 DOWNTO 0);
     signal spi_loader_mem_write : std_logic;
 
-    -- Sinal do botão (switch) para controle de gravação
     signal switch_enable, s_resdet : std_logic;
 
 BEGIN
-    -- =========================================================================
-    -- == INSTANCIAÇÃO DOS COMPONENTES                                        ==
-    -- =========================================================================
-
-    -- Instancia a Unidade de Controle (UC)
     uc_inst : ahmes_uc
         PORT MAP(
-            address_bus => s_address_bus,
+            address_bus => uc_address_bus,
             data_in     => s_data_from_mem,
-            data_out    => s_data_to_mem,
-            mem_write   => alu_mem_write,
+            data_out    => uc_data_to_mem,
+            mem_write   => uc_mem_write,
             clk         => clk,
             reset       => s_resdet,
             btns        => btns,
@@ -139,7 +120,6 @@ BEGIN
             V           => s_v
         );
 
-    -- Instancia a Unidade Lógica e Aritmética (ALU)
     alu_inst : ALU
         PORT MAP(
             operacao => std_logic_vector(s_operacao),
@@ -154,42 +134,32 @@ BEGIN
             V        => s_v
         );
 
-    -- Instancia a Memória
     mem_inst : memoria
         PORT MAP(
             address_bus 	=> s_address_bus,
-            data_in     		=> s_data_to_mem,
+            data_in     	=> s_data_to_mem,
             data_out   	 	=> s_data_from_mem,
             mem_write   	=> s_mem_write,
-            clk         		=> s_mem_clk,
-            rst         		=> reset
+            clk         	=> clk,
+            rst         	=> reset
         );
 
-    -- Instancia o SPI Loader
     spi_loader_inst : spi_loader
         PORT MAP (
             clk            => clk,
-            spi_sck        => spi_sck,  -- Conexão direta para SCK SPI
-            spi_ss         => spi_ss,   -- Conexão direta para SS SPI
-            spi_mosi       => spi_mosi, -- Conexão direta para MOSI SPI
-            spi_addr_bus   => spi_loader_addr_bus,
+            spi_sck        => spi_sck, 
+            spi_ss         => spi_ss,   
+            spi_mosi       => spi_mosi, 
             spi_data_out   => spi_loader_data_out,
+				spi_addr_bus	=> spi_loader_addr_bus,
             spi_mem_write  => spi_loader_mem_write
         );
 
-    -- =========================================================================
-    -- == LÓGICA DE CONTROLE DO BOTÃO PARA GRAVAÇÃO                          ==
-    -- =========================================================================
-    switch_enable <= btns(0);  
-	s_resdet <=switch_enable;
+	-- Memory SPI write
+   switch_enable 	<=  btns(0);  
+	s_resdet 		<=  switch_enable;
+   s_mem_write 	<=  uc_mem_write  	when switch_enable = '0' else spi_loader_mem_write;
+	s_address_bus 	<=  uc_address_bus   when switch_enable = '0' else spi_loader_addr_bus;
+	s_data_to_mem 	<=  uc_data_to_mem	when switch_enable = '0' else spi_loader_data_out;
 	
-    -- =========================================================================
-    -- == LÓGICA DE CONTROLE DA GRAVAÇÃO NA MEMÓRIA                          ==
-    -- =========================================================================
-    -- Gravação na memória é habilitada se o switch estiver ativado e o
-    -- SPI Loader tiver sinal de mem_write ativo.
-	
-    s_mem_write <= alu_mem_write  when switch_enable = '0' else spi_loader_mem_write;
-	s_mem_clk 	<= clk when switch_enable = '0' else spi_sck; 
-
 END ARCHITECTURE structural;
